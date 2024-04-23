fb326a0a93ff5945d21844263a0619fc52a7bd64
MirzaMlna <mirzamaulana713@gmail.com>
2024-04-23 21:00:16 +0800

    ⏪ Revert Password Toogle Feature

--- lib/widgets/app_text_field.dart
+++ lib/widgets/app_text_field.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:mobile_user/app/theme/app_colors.dart';
 import 'package:mobile_user/app/theme/app_text_style.dart';
 
@@ -15,7 +16,7 @@ class AppTextField extends StatefulWidget {
   final bool enabled;
 
   const AppTextField({
-    Key? key,
+    super.key,
     required this.labelText,
     this.hintText,
     this.labelStyle,
@@ -26,58 +27,44 @@ class AppTextField extends StatefulWidget {
     this.border,
     this.borderRadius,
     this.enabled = true,
-  }) : super(key: key);
+  });
 
   @override
   State<AppTextField> createState() => _AppTextFieldState();
 }
 
 class _AppTextFieldState extends State<AppTextField> {
-  bool _isObscured = true;
-
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       controller: widget.controller,
       keyboardType: widget.keyboardType,
-      obscureText: _isObscured,
+      obscureText: widget.obscureText,
       decoration: InputDecoration(
-        enabled: widget.enabled ? widget.enabled : false,
-        floatingLabelBehavior: FloatingLabelBehavior.always,
-        floatingLabelStyle: AppTextStyle.regular(size: 20),
-        labelText: widget.labelText,
-        hintText: widget.hintText,
-        labelStyle: widget.labelStyle ?? TextStyle(color: AppColors.secondary),
-        hintStyle: widget.hintStyle,
-        enabledBorder: widget.border ??
-            OutlineInputBorder(
-              borderSide: BorderSide(color: AppColors.secondary, width: 1.5),
-              borderRadius: widget.borderRadius ?? BorderRadius.circular(10),
-            ),
-        disabledBorder: widget.border ??
-            OutlineInputBorder(
-              borderSide: BorderSide(color: AppColors.secondary, width: 1.5),
-              borderRadius: widget.borderRadius ?? BorderRadius.circular(10),
-            ),
-        focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: AppColors.primary, width: 1.5),
-          borderRadius: widget.borderRadius ?? BorderRadius.circular(10),
-        ),
-        focusColor: AppColors.primary,
-        suffixIcon: widget.obscureText
-            ? IconButton(
-                onPressed: () {
-                  setState(() {
-                    _isObscured = !_isObscured;
-                  });
-                },
-                icon: Icon(
-                  _isObscured ? Icons.visibility_off : Icons.visibility,
-                  color: AppColors.secondary,
-                ),
-              )
-            : null,
-      ),
+          enabled: widget.enabled ? widget.enabled : false,
+          floatingLabelBehavior: FloatingLabelBehavior.always,
+          floatingLabelStyle: AppTextStyle.regular(size: 20),
+          labelText: widget.labelText,
+          hintText: widget.hintText,
+          labelStyle:
+              widget.labelStyle ?? TextStyle(color: AppColors.secondary),
+          hintStyle: widget.hintStyle,
+          enabledBorder: widget.border ??
+              OutlineInputBorder(
+                  borderSide:
+                      BorderSide(color: AppColors.secondary, width: 1.5),
+                  borderRadius:
+                      widget.borderRadius ?? BorderRadius.circular(10)),
+          disabledBorder: widget.border ??
+              OutlineInputBorder(
+                  borderSide:
+                      BorderSide(color: AppColors.secondary, width: 1.5),
+                  borderRadius:
+                      widget.borderRadius ?? BorderRadius.circular(10)),
+          focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: AppColors.primary, width: 1.5),
+              borderRadius: widget.borderRadius ?? BorderRadius.circular(10)),
+          focusColor: AppColors.primary),
     );
   }
 }
