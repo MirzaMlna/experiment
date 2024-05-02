26ef2231064ae8c410cb36260fe26788a0ce4aad
MirzaMlna <mirzamaulana713@gmail.com>
2024-05-02 23:12:13 +0800

    💄 Updating Color Theme

--- lib/app/theme/app_colors.dart
+++ lib/app/theme/app_colors.dart
@@ -39,9 +39,9 @@ class AppColors {
   // Named Colors
   static const Color primary = blueLv1;
   static const Color secondary = blueLv6;
-  static const Color success = greenLv2;
-  static const Color warning = yellowLv2;
-  static const Color error = redLv2;
+  static const Color success = greenLv1;
+  static const Color warning = yellowLv1;
+  static const Color error = redLv1;
   static const Color disabled = blackLv5;
 
   // Black
